db516485f2c43bd01d84aaacabeea809e260685f
Crhistian Beteta <crhistian.beteta@alu.uclm.es>
2023-10-07 20:24:50 +0200

    fix: edit npm scripts and remove comments

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import express, { json } from "express";
 import generatorRoutes from './routes/qr-generator.routes';
 const app = express();
 const port = 3000;
-console.log("directio", __dirname)
+
 app.use(express.urlencoded({ extended: true }));
 app.set('views', __dirname + '/views')
-app.set('view engine', 'pug'); // Configurar Pug como el motor de vistas
-//app.use('/static', express.static(__dirname + '/public'));
+app.set('view engine', 'pug');
+
 app.use(express.static('public'));
 app.use(json())
 
